2c04707bce3c79a01bce546d1ec343265cfd1b41
rhonamackay <rhonamackay@outlook.com>
2022-12-06 14:08:53 +0000

    Headers removed from del fetch req

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -52,8 +52,7 @@ function App() {
   async function clearList() {
     //This function clears all the items that have been added to the list.
     const response = await fetch(`${url}/items`, {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
+      method: "DELETE"
     });
     console.log(response)
     const clearedList = [];
